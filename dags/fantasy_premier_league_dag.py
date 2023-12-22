271e9b1290d3ddb000bae1b328b41a73e5ebde9a
azfar-mustafa <khairulbakar95@gmail.com>
2023-12-22 17:29:49 +0800

    Add function for silver container archive

--- dags/fantasy_premier_league_dag.py
+++ dags/fantasy_premier_league_dag.py
@@ -161,8 +161,8 @@ def get_old_date(**kwargs):
     date_counts = set()
     for path in list_of_files:
         parts = path.split('/')
-        if len(parts) > 1 and parts[1].isdigit():
-            date_counts.add(parts[1])
+        if len(parts) > 2 and parts[2].isdigit() and "current" in parts:
+            date_counts.add(parts[2])
 
     datetime_dates = [datetime.strptime(date, "%d%m%Y") for date in date_counts]
     min_date = min(datetime_dates)
@@ -430,6 +430,96 @@ def teams_metadata_bronze_to_silver():
     print(f"{temp_dir} is removed")
 
 
+def get_silver_old_date(**kwargs):
+    container_name = 'silver'
+    az_hook = WasbHook(wasb_conn_id=AZURE_BLOB_CONN_ID)
+    list_of_files = az_hook.get_blobs_list_recursive(container_name=container_name) #To get the first level blob name
+    print(f"List of files - {list_of_files}")
+    date_counts = set()
+    for path in list_of_files:
+        parts = path.split('/')
+        print(f"Parts - {parts}")
+        if len(parts) > 2 and parts[2].isdigit() and "current" in parts:
+            date_counts.add(parts[2])
+
+    datetime_dates = [datetime.strptime(date, "%d%m%Y") for date in date_counts]
+    print(datetime_dates)
+    min_date = min(datetime_dates)
+    non_recent_date = min_date.strftime("%d%m%Y")
+    print(non_recent_date)
+
+    kwargs['ti'].xcom_push(key='my_key', value=non_recent_date)
+
+
+def move_silver_file_into_archive_folder(**kwargs):
+    ti = kwargs['ti']
+    formatted_current_date = ti.xcom_pull(task_ids='get_silver_old_date', key='my_key')
+
+    print(f"value {formatted_current_date}")
+    
+    container_name = 'silver'
+    
+    az_hook = WasbHook(wasb_conn_id=AZURE_BLOB_CONN_ID)
+    list_of_files = az_hook.get_blobs_list(container_name=container_name) #To get the first level blob name
+    new_list_of_files = [original_string.replace('/', '') for original_string in list_of_files]
+    print("File is renamed")
+    print(new_list_of_files)
+ 
+    for folder_name in  new_list_of_files:
+        print(folder_name)
+        blob_name = f"{folder_name}_{formatted_current_date}.parquet"
+        blob_path = f"{folder_name}/current/{formatted_current_date}/{blob_name}"
+        virtual_folder_path = f"{folder_name}/archive/{formatted_current_date}/"
+
+
+        temp_dir = tempfile.mkdtemp()
+        print("Created local temporary folder")
+        print(blob_path)
+        temp_file_path = os.path.join(temp_dir, blob_name)
+
+        az_hook = WasbHook(wasb_conn_id=AZURE_BLOB_CONN_ID)
+        az_hook.get_file(file_path=temp_file_path, container_name=container_name, blob_name=blob_path)
+        print(f"File is downloaded at {temp_dir}")
+
+        az_hook.load_file(
+                    file_path=temp_file_path,
+                    container_name=container_name,
+                    blob_name=f"{virtual_folder_path}{blob_name}",
+                    overwrite=True
+                )
+        print(f"File is copied to archive")
+
+
+def delete_old_file_in_silver_folder(**kwargs):
+    # Reference
+    # https://airflow.apache.org/docs/apache-airflow-providers-microsoft-azure/stable/_api/airflow/providers/microsoft/azure/hooks/data_lake/index.html
+    # https://airflow.apache.org/docs/apache-airflow-providers-microsoft-azure/stable/_api/airflow/providers/microsoft/azure/hooks/wasb/index.html
+
+    ti = kwargs['ti']
+    formatted_current_date = ti.xcom_pull(task_ids='get_silver_old_date', key='my_key')
+
+    container_name = 'silver'
+
+    file_system_name = 'silver'
+    
+
+    # Delete directory and the file
+
+    az_hook = WasbHook(wasb_conn_id=AZURE_BLOB_CONN_ID)
+    list_of_files = az_hook.get_blobs_list(container_name=container_name) #To get the first level blob name
+    new_list_of_files = [original_string.replace('/', '') for original_string in list_of_files]
+
+    for folder_name in new_list_of_files:
+        directory_to_delete = f"{folder_name}/current/{formatted_current_date}"
+        adls_hook = AzureDataLakeStorageV2Hook(adls_conn_id=AZURE_BLOB_CONN_ID)
+        print("Client is created")
+        try:
+            adls_hook.delete_directory(file_system_name, directory_to_delete)
+            print(f"Folder '{directory_to_delete}' deleted successfully.")
+        except Exception as e:
+            print(f"Fail to delete because of {str(e)}")
+
+
 
 fetch_fpl_api_data_task = SimpleHttpOperator(
     task_id='fetch_fpl_api_data',
@@ -516,12 +606,36 @@ teams_metadata_bronze_to_silver = PythonOperator(
     dag=dag,
 )
 
+
+get_silver_old_date = PythonOperator(
+    task_id='get_silver_old_date',
+    python_callable=get_silver_old_date,
+    provide_context=True,
+    dag=dag,
+)
+
+
+move_silver_file_into_archive_folder = PythonOperator(
+    task_id='move_silver_file_into_archive_folder',
+    python_callable=move_silver_file_into_archive_folder,
+    provide_context=True,
+    dag=dag,
+)
+
+
+delete_old_file_in_silver_folder = PythonOperator(
+    task_id='delete_old_file_in_silver_folder',
+    python_callable=delete_old_file_in_silver_folder,
+    provide_context=True,
+    dag=dag,
+)
+
 # Reference
 # https://cloudshuttle.com.au/blog/2022-10/setting_up_airflow
 # https://airflow.apache.org/docs/apache-airflow-providers-microsoft-azure/stable/_api/airflow/providers/microsoft/azure/hooks/wasb/index.html
 # https://dzone.com/articles/simplehttpoperator-in-apache-airflow
 
-fetch_fpl_api_data_task >> extract_data_from_api_task >> upload_to_blob_task >> download_file_from_blob_task  >> get_old_date >> move_bronze_file_into_archive_folder >> delete_file_in_actual_folder >> [current_season_history_bronze_to_silver, player_metadata_bronze_to_silver, position_metadata_bronze_to_silver, teams_metadata_bronze_to_silver]
+fetch_fpl_api_data_task >> extract_data_from_api_task >> upload_to_blob_task >> download_file_from_blob_task  >> get_old_date >> move_bronze_file_into_archive_folder >> delete_file_in_actual_folder >> [current_season_history_bronze_to_silver, player_metadata_bronze_to_silver, position_metadata_bronze_to_silver, teams_metadata_bronze_to_silver] >> get_silver_old_date >> move_silver_file_into_archive_folder >> delete_old_file_in_silver_folder
 
 if __name__ == "__main__":
     dag.cli()
